1d8ab3dd8ccad3b6b98966cd0904647e16d8044e
Herve08 <151453343+Herve08@users.noreply.github.com>
2024-05-04 16:10:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let firstName = document.querySelector('#first-name').value;
+    let lastName = document.querySelector('#last-name').value;
+    let message = document.querySelector('#message').value;
+    let errormessage = document.querySelector('#error-message');
+
+    if (firstName === '' || lastName === '' || message === '') {
+        errormessage.style.display = "block";
+    } else {
+        errormessage.style.display = "none";
+
+        newComment = document.createElement('div');
+        newComment.classList.add("flex", "space-x-4", "text-sm", "text-gray-500");
+        newComment.innerHTML = `
+        <div class="flex-1 py-10 border-t border-gray-200">
+                  <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
+                  <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                    <p>${message}</p>
+                  </div>
+                </div>
+        `;
+
+        let commentList = document.querySelector('#comment-list');
+        commentList.appendChild(newComment);
+
+        form.reset();
+    }
+});
